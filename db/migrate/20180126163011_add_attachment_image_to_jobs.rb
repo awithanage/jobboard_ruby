0709ba036abef2994e75042d669e7765a8c46e00
awithanage <kethianuradha@gmail.com>
2018-02-05 16:51:09 +0530

    added changes to db attachment files

--- db/migrate/20180126163011_add_attachment_image_to_jobs.rb
+++ db/migrate/20180126163011_add_attachment_image_to_jobs.rb
@@ -1,4 +1,4 @@
-class AddAttachmentImageToJobs < ActiveRecord::Migration[5.0.0]
+class AddAttachmentImageToJobs < ActiveRecord::Migration[5.1]
   def self.up
     change_table :jobs do |t|
       t.attachment :image
